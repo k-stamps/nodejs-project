74c5965a205153576607d7d8a06f1847dcc6365a
kstamps <kstampstech@gmail.com>
2025-04-21 16:28:29 -0400

    adding react client

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
+const cors = require('cors');
+
+app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World, this is the node server side.')
 })
 
 app.listen(port, () => {
